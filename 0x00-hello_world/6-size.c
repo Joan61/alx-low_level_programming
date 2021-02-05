2b0e78f040fe09af906258405a3b49863831d672
Joan Mbarire <jojohaki61@gmail.com>
2021-02-05 11:07:44 +0000

    puts

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main-prints standard variable size to stdout
- *description:uses sizeof to determine variable size
+ * description:uses sizeof to determine variable size
  * Return:void
 **/
 int main(void)
