97502c26dda0c8c97da9a5841427b9de449e91ea
Joan Mbarire <jojohaki61@gmail.com>
2021-02-05 10:49:30 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-**
-* main-prints standard variable size to stdout
-* description:uses sizeof to determine variable size
-* Return:void
-*/
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
