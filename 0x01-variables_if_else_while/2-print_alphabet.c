0d1e80aeb1deba8d2b9a999e68f1c42d55918013
Joan Mbarire <jojohaki61@gmail.com>
2021-02-07 11:20:26 +0000

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - where execution starts
+*
+*Description: The program will print the alphabets from a-z
+*Return: 0 if successful
+**/
+int main(void)
+{
+char ch = 'a';
+
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
