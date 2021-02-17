45a2bc6309c95cdf20f188dcefa61adccd88db1c
joan61 <jojohaki61@gmail.com>
2021-02-17 13:49:05 +0000

    revindex

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+*rev_string: Function that reverses a string
+*@s: string to be reversed
+*
+*Return: void
+**/
+void rev_string(char *s)
+{
+int index = 0;
+int revindex =0;
+int temp;
+char *origin = s;
+
+while(*s != '\0')
+{
+revindex ++;
+s++;
+}
+revindex--;
+
+while(index <= revindex)
+{
+temp = *(origin + index);
+*(origin + index) = *(origin + revindex);
+*(origin + revindex) = temp;
+index++;
+revindex--;
+}
+}
