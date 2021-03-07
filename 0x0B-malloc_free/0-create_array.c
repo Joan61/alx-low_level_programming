77f962e6c4654b8cec939b3ba37d2a95e1f5d403
joan61 <jojohaki61@gmail.com>
2021-03-07 19:25:58 +0000

    ptr

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,31 +1,31 @@
 #include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
 /**
-*create_array - creates an array of chars and initializes it with char c
-*@size: number of chars to reserve memory for
-*@c: char to initialize array
-*Return: pointer to char or NULL if size is 0
+*create_array - creates an array of chars and initializes with specific char
+*@size: size of
+*@c: character to initialize it with
+*
+*Return: pointer to array
 **/
 char *create_array(unsigned int size, char c)
 {
-char *ptr;
-
-unsigned int i;
+unsigned int i = 0;
+char *chararray;
 
 if (size == 0)
-{
-return (0);
-}
-else
-{
-ptr = (char *) malloc(size * sizeof(char));
+return (NULL);
 
-for (i = 0; i < size; i++)
-{
-ptr[i] = c;
-}
-ptr[i] = '\0';
+chararray = malloc(sizeof(char) * size);
 
+if (chararray == NULL)
+return (NULL);
+
+while (i < size)
+{
+*(chararray + i) = c;
+i++;
 }
-return (ptr);
+
+return (chararray);
 }
