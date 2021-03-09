a8e18da5d618144f6fc0fd598480667181e66034
joan61 <jojohaki61@gmail.com>
2021-03-09 19:59:16 +0000

    first commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,19 +11,17 @@
 
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 typedef struct dog dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-  if (d != NULL)
-    {
-      d->name = name;
-      d->age = age;
-      d->owner = owner;
-    }
-}
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+void free_dog(dog_t *d);
+#endif
